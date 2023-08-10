01249dfb443fa17f0ff78f6ea69ca22509b1918c
Kseniya7704 <ksyusha.ananeva.92@gmail.com>
2023-08-10 19:41:36 +0300

    upload

--- src/route/index.js
+++ src/route/index.js
@@ -23,6 +23,33 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    title: 'Resume',
+
+    name: {
+      firstname: 'Ivan',
+      lastname: 'Ivanov',
+    },
+    position: 'Junior Fullstack JS Developer',
+
+    salary: {
+      text: 'Desired salary',
+      value: '600$ в місяць',
+    },
+
+    social: {
+      email: {
+        text: 'dmytro@mail.com',
+        href: 'mailto:dmytro@mail.com',
+      },
+      phone: {
+        text: '+380670000123',
+        href: 'tel:+380670000123',
+      },
+      link: {
+        text: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/dmytro-test',
+      },
+    },
   })
 })
 
