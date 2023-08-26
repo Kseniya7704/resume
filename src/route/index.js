4ebe0b08dd1c347294e609c97ad06638a26d15bd
Kseniya7704 <ksyusha.ananeva.92@gmail.com>
2023-08-26 19:37:52 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяць',
+  address: 'Київ, проспект Перемоги, 54/1',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,33 +52,223 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
-    title: 'Resume',
-
-    name: {
-      firstname: 'Ivan',
-      lastname: 'Ivanov',
-    },
-    position: 'Junior Fullstack JS Developer',
-
-    salary: {
-      text: 'Desired salary',
-      value: '600$ в місяць',
+    page: {
+      title: 'Resume | Summary',
     },
 
-    social: {
-      email: {
-        text: 'dmytro@mail.com',
-        href: 'mailto:dmytro@mail.com',
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
       },
-      phone: {
-        text: '+380670000123',
-        href: 'tel:+380670000123',
-      },
-      link: {
-        text: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/dmytro-test',
+
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics ( tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc',
       },
     },
+    footer,
+  })
+})
+// Підключаємо роутер до бек-енду
+module.exports = router
+
+// ================================================================
+
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 8,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 9,
+          isTop: true,
+        },
+        {
+          name: 'Git',
+          point: 7,
+          isTop: false,
+        },
+        {
+          name: 'Terminal',
+          point: 10,
+        },
+        {
+          name: 'NPM',
+          point: 9,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Traveling',
+          isMain: true,
+        },
+        {
+          name: 'Playing board games',
+          isMain: true,
+        },
+        {
+          name: 'Photography',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+// Підключаємо роутер до бек-енду
+module.exports = router
+
+// ================================================================
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'Інститут банківських технологій та бізнесу',
+          isEnd: true,
+        },
+        {
+          name: 'Інститут міжнародних відносин Київського національного університету імені Тараса Шевченка',
+          isEnd: true,
+        },
+        {
+          name: 'Київська академія міжнародної економіки і міжнародних відносин (КАМЕМВ)',
+          isEnd: false,
+        },
+        {
+          name: 'Київський інститут бізнесу та технологій (КІБІТ)',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'Java Foundations',
+          id: '7865423695',
+        },
+        {
+          name: 'Unity Certified Professional Programmer',
+          id: '7125746399',
+        },
+        {
+          name: 'JavaScript Certification (CJSD)',
+          id: '76397542877',
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+// Підключаємо роутер до бек-енду
+module.exports = router
+
+// ================================================================
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          project: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/',
+              about: 'Project without borders.',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Proposed an idea that made the site more user-friendly',
+                },
+                {
+                  name: 'Improved the structure of the company website',
+                },
+              ],
+              stackAmount: 7,
+              awardAmount: 5,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
   })
 })
 
